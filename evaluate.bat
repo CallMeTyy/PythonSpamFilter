3f2c81bae310c267bf6b2d61b3f7b218355c1bbf
Tycho Tuitert <500805@student.saxion.nl>
2023-06-06 11:36:13 +0200

    Removed bat files from git

--- evaluate.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Input Folder with test files
-set /p input= 
-echo Input checkpoint path
-set /p inputckpt= 
-"python3" "evaluate.py" "--folder" "%input%" "--checkpoint" "%inputckpt%" "--checkhamspam" "True"
-pause
--- train.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Input Folder to Train
-set /p input= 
-echo Input Class Amount
-set /p inputamt= 
-echo Input Vocabulary Amount
-set /p inputv= 
-"python3" "train.py" "--folder" "%input%" "--c" "%inputamt%" "--v" "%inputv%"
-pause
